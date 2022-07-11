437160382e7bbb1849c55a68c7f6ccce3cf45468
Leandro Amorim <leandroamorimpb@gmail.com>
2022-07-11 20:23:08 -0300

    Criando o método excluir fornecedor :beetle:

--- financeiro/src/test/java/br/com/ats/financeiro/dao/testes/FornecedorDaoTeste.java
+++ financeiro/src/test/java/br/com/ats/financeiro/dao/testes/FornecedorDaoTeste.java
@@ -145,4 +145,33 @@ public class FornecedorDaoTeste {
 			System.out.println("-----------------------------------------------------------------");
 		}
 	}
+	
+	@Test
+	@Ignore
+	public void excluir() throws Excecoes{
+		
+		Long codigo = 1L;
+		FornecedorDao dao = new FornecedorDao();
+		Fornecedor fornecedor = dao.buscarPorId(codigo);
+		
+		SimpleDateFormat data = new SimpleDateFormat("dd-MM-yyyy");
+		SimpleDateFormat dataEHora = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		if (fornecedor == null) {
+			System.out.println("Nenhum fornecedor encontrado com código " + codigo + "!");
+		} else {
+			dao.excluir(fornecedor);
+			System.out.println("Fornecedor excluído com sucesso!");
+			System.out.println(
+					"\nRazão Social : " + fornecedor.getRazaoSocial() 
+					+ "\nNome Fantasia : " + fornecedor.getNomeFantasia() 
+					+ "\nCNPJ : " + fornecedor.getCnpj() 
+					+ "\nEmail : " + fornecedor.getEmail() 
+					+ "\nInsc. Estadual : " + fornecedor.getInscEstadual()
+					+ "\nInsc. Municipal : " + fornecedor.getInscMunicipal() 
+					+ "\nData Cadastro : " + dataEHora.format(fornecedor.getDataCadastrado()) 
+					+ "\nData Fundação  : " + data.format(fornecedor.getDataFundacao()));
+			System.out.println("-----------------------------------------------------------------");
+		}
+	}
+	
 }
